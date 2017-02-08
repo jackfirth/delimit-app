e81c362e2a4155e2c7673b1390884b2e87d077d4
Jack Firth <jackhfirth@gmail.com>
2017-02-07 18:25:10 -0800

    Require Racket version 6.4 or later

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "delimit-app")
 (define scribblings '(("main.scrbl" () (library) "delimit-app")))
-(define deps '("base"
+(define deps '(("base" #:version "6.4")
                "fancy-app"))
 (define build-deps '("rackunit-lib"))
